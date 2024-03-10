250c1637618d87a6db25d79213f7f470e8213046
JakubLodonDev <lodon.jakub@gmail.com>
2024-03-10 20:20:50 +0100

    Refactoror code and Add interfaces

--- flashcards/src/main/java/app/jakub/flashcards/Service/CardService.java
+++ flashcards/src/main/java/app/jakub/flashcards/Service/CardService.java
@@ -1,28 +1,12 @@
 package app.jakub.flashcards.Service;
 
 import app.jakub.flashcards.model.Card;
-import app.jakub.flashcards.repo.CardRepository;
-import app.jakub.flashcards.repo.DeckRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CardService {
-    CardRepository cardRepository;
-    DeckRepository deckRepository;
-
-    @Autowired
-    public CardService(CardRepository cardRepository, DeckRepository deckRepository) {
-        this.cardRepository = cardRepository;
-        this.deckRepository = deckRepository;
-    }
-    public Card saveCard(Card card) {
-        return cardRepository.save(card);
-    }
-
-    public List<Card> findAllCardsByDeckId(Long deckId) {
-        return cardRepository.findCardsByDeck_DeckId(deckId);
-    }
+public interface CardService {
+    Card saveCard(Card card);
+    List<Card> findAllCardsByDeckId(Long deckId);
 }
--- flashcards/src/main/java/app/jakub/flashcards/Service/DeckService.java
+++ flashcards/src/main/java/app/jakub/flashcards/Service/DeckService.java
@@ -1,32 +1,19 @@
 package app.jakub.flashcards.Service;
 
 import app.jakub.flashcards.model.Deck;
-import app.jakub.flashcards.repo.DeckRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class DeckService {
+public interface DeckService {
 
-    @Autowired
-    DeckRepository deckRepository;
+    Optional<Deck> findDeckById(Long id);
 
-    public Optional<Deck> findDeckById(Long id) {
-        return deckRepository.findById(id);
-    }
+    List<Deck> findAllDecks();
 
-    public List<Deck> findAllDecks() {
-        return deckRepository.findAll();
-    }
+    void deleteDeck(Long deckId);
 
-    public void deleteDeck(Long deckId) {
-        deckRepository.deleteById(deckId);
-    }
-
-    public Deck saveNewDeck(Deck deck) {
-        return deckRepository.save(deck);
-    }
+    Deck saveNewDeck(Deck deck);
 }
